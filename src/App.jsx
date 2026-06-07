cfceb6b04345467df5f4f7983a23383af4aa6e1e
sabya <sabyasachee01@gmail.com>
2026-06-07 11:10:24 +0530

    fix: align blog routing with UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,41 @@
 import { useState } from 'react'
 import Home from './home.jsx'
 import './App.css'
-import Login from './assets/account.jsx'
 
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
-import Blogs from './blog.jsx';
 import Account from './assets/account.jsx'
-import Service from './service.jsx';
+import Blogs from './blog.jsx'
+import Service from './service.jsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 function App() {
-  
+  const [themes] = useState(false)
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home title="Kanha Saloon" />,
+    },
+    {
+      path: '/account',
+      element: <Account />,
+    },
+    {
+      path: '/blogs',
+      element: <Blogs />,
+    },
+    {
+      path: '/services',
+      element: <Service />,
+    },
+  ])
 
-const router= createBrowserRouter([
-
-  {
-    path: '/',
-    element: <Home title="Kanha Saloon"  />,
-  },
-  { 
-    path:"/account",
-    element:<Account/>
-  },
-{
-  path:"/blogs/:id",
-  element:<Blogs/>
-},
-{
-  path:"/services",
-  element:<Service/>
-},
-
-])
-  
-let [themes, setThemes] = useState(false);
   return (
-    < >
-   
-
-    <div
-      className={`min-h-screen transition-colors duration-500 ${
-        themes ? 'bg-black text-white' : 'bg-white text-black'
-      }`}
-    >
+    <div className={`min-h-screen transition-colors duration-500 ${
+      themes ? 'bg-black text-white' : 'bg-white text-black'
+    }`}>
       <RouterProvider router={router} />
-
-      
     </div>
-
-    
-    
-  
-    </>
   )
 }
 
--- src/blog.jsx
+++ src/blog.jsx
@@ -1,40 +1,28 @@
-import { blogs } from "./assets/blogs";
+import { blogs } from "./assets/blogs"
 
-import { Link } from "react-router-dom";
-function Blogs(){  
+function Blogs() {
+  return (
+    <>
+      <h1>Blog page</h1>
 
+      {blogs.map((v, i) => (
+        <div
+          key={v.id || i}
+          className="border-2 border-gray-300 rounded-lg p-4 m-4 shadow-lg hover:shadow-xl transition-shadow duration-300"
+        >
+          <p>
+            {v.title}
+            <br />
+            {v.body}
+          </p>
 
-let allblogs=blogs.map((v,i)=>{
+          <button className="rounded border-blue-600">
+            Read more
+          </button>
+        </div>
+      ))}
+    </>
+  )
+}
 
-    return(
-<>
-
-        <div className="border-2  border-gray-300 rounded-lg p-4 m-4 shadow-lg hover:shadow-xl transition-shadow duration-300" >
-        <div key={i}>
-            <p>
-                {v.title}
-                {v.body}
-               
-            </p>
-            
-                  <button className="rounded border-blue-600 hover:animate-ping"><Link to={`/blogs/${v.id}`}>Read more</Link></button>
-       
-        </div></div>
-        
-
-</>
-      
-    )
-
-})
-  
-
-return(
-<>
-<h1>Blog page</h1>
-
-{allblogs}
-</>
-
-)
-}export default Blogs;
+export default Blogs
